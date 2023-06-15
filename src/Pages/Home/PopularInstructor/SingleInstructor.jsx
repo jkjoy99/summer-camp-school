704df3f4df12ed8d40ef2c339e7020dde3194e92
jkjoy99 <jkjoy99@gmail.com>
2023-06-15 20:55:20 +0600

    popularclass insart

---
+++ src/Pages/Home/PopularInstructor/SingleInstructor.jsx
@@ -0,0 +1,22 @@
+const SingleInstructor = ({ allInstructor }) => {
+  const { name, photo, email } = allInstructor;
+
+  return (
+    <div>
+      <div className="card w-96 h-[400px] bg-base-100 shadow-xl">
+        <figure>
+          <img src={photo} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            Name: {name}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>Email: {email}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleInstructor;
